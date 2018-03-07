f08d2c7fd91dd3829a241412d06e032a08bae5d9
Tinashe Dre Chafanza <smartstudios89@outlook.com>
2018-03-07 16:12:05 +0200

    Contact App Vue

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+
+  apiKey: "AIzaSyCw72jo6R1fqA4k1ONV-JjJ-TsfYF25E_s",
+  authDomain: "apollo-d1bad.firebaseapp.com",
+  databaseURL: "https://apollo-d1bad.firebaseio.com",
+  projectId: "apollo-d1bad",
+  storageBucket: "apollo-d1bad.appspot.com",
+  messagingSenderId: "551533234363"
+}
